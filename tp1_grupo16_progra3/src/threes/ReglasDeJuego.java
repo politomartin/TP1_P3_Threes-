65b67e67aab48f0c32ff9459c0143c0b70339eeb
Martin Polito <Martin Polito@NBLP266>
2026-09-07 14:58:23 -0300

    Agregado de funciones faltantes :p

--- tp1_grupo16_progra3/src/threes/ReglasDeJuego.java
+++ tp1_grupo16_progra3/src/threes/ReglasDeJuego.java
@@ -62,11 +62,31 @@ public class ReglasDeJuego {
 	
 	
 	private void agregarFichaNuevaArribaYAbajo(int fila) {
-		//Al momento en desarrollo
+		if(puedoAgregarFichaArribaOAbajo(fila)) {
+			int col = random.nextInt(3);
+			boolean iterador = true;
+			while(iterador) {
+				if(posicionLibre(fila, col)) {
+					tablero.setValor(fila, col, siguienteFicha);
+					iterador = false;
+				}
+				col = random.nextInt(3);
+			}
+		}
 	}
 	
-	private void agregarFichaNuevaIzquierdaODerecha(int referencia) {
-		//Al momento en desarrollo		
+	private void agregarFichaNuevaIzquierdaODerecha(int col) {
+		if(puedoAgregarFichaIzquierdaODerecha(col)) {
+			int fila = random.nextInt(3);
+			boolean iterador = true;
+			while(iterador) {
+				if(posicionLibre(fila, col)) {
+					tablero.setValor(fila, col, siguienteFicha);
+					iterador = false;
+				}
+				col = random.nextInt(3);
+			}
+		}
 	}
 	//
 	//Apartado del moviento de la ficha 
